24f26710235e2d25b3aad2a26bbc77736962ad6d
Angela Melinda <angela.melinda@gmail.com>
2020-01-05 01:19:31 +0700

    Initial Commit

---
+++ src/config/data.ts
@@ -0,0 +1,70 @@
+export const PROFILE_INFORMATION = {
+  NAME: "Angela Melinda",
+  JOB: "Consult, Design, and Develop Website"
+};
+
+export const NOTIFICATION_PANEL = {
+  BUTTON: "Got It"
+};
+
+export const HERO = {
+  BUTTON: "Let's Make Contact",
+  TAGLINE: `Hello! I'm ${PROFILE_INFORMATION.NAME}`,
+  SUBTITLE: {
+    LINE_1: "Have something great in mind? Feel free to contact me.",
+    LINE_2: "I'll help you to make it happen."
+  }
+};
+
+export const HIGHLIGHTS_PANEL = {
+  TITLE: "How Can I Help You?",
+  SUBTITLE:
+    "Our work then targeted, best practices outcomes social innovation synergy. Venture philanthropy, revolutionary inclusive policymaker relief. User-centered program areas scale.",
+  JOB_DESCRIPTION: [
+    {
+      TITLE: "Consult",
+      CONTENT:
+        "Co-create, design thinking; strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.",
+      ICON: "comments"
+    },
+    {
+      TITLE: "Design",
+      CONTENT:
+        "Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile  issue outcomes vibrant boots on the ground sustainable.",
+      ICON: "paint-brush"
+    },
+    {
+      TITLE: "Develop",
+      CONTENT:
+        "Revolutionary circular, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.",
+      ICON: "cubes"
+    },
+    {
+      TITLE: "Marketing",
+      CONTENT:
+        "Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.",
+      ICON: "bullhorn"
+    },
+    {
+      TITLE: "Manage",
+      CONTENT:
+        "Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.",
+      ICON: "poll-h"
+    },
+    {
+      TITLE: "Evolve",
+      CONTENT:
+        "Activate catalyze and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes big data, fairness, social game-changer.",
+      ICON: "chart-line"
+    }
+  ]
+};
+
+export const NEWSLETTER_PANEL = {
+  TITLE: "Get latest updates in web technologies",
+  SUBTITLE:
+    "I write articles related to web technologies, such as design trends, development tools, UI/UX case studies and reviews, and more. Sign up to my newsletter to get them all.",
+  FORM_CONTACT: {
+    BUTTON: "Count Me In!"
+  }
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+export const COLOR = {
+  BLUE: "#007bc1",
+  DARK_BLUE: "#004a75",
+  ORANGE: "#ff8000",
+  DARK_ORANGE: "#cc6600",
+  SMOKE_GREY: "#e5e5e5"
+};
+
+export const CONTAINERS = {
+  WIDER_CONTENT: 1366,
+  CONTENT: 720,
+  PANEL_SIZE: 640
+};
+
+export const MEDIA_SCREEN = {
+  SMALL: 480
+};
+
+export const NOTIFICATION_PANEL_TIMEOUT = 10 * 60 * 1000;
